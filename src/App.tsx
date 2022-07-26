fc5a4ad2983be784992cb029d570a31c9d956442
Viktor <viktor.p@li.solutions>
2022-07-26 11:09:42 +0300

    tw add

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import ProgressBar from './components/ProgressBar';
 function App() {
   return (
     <div className=" px-8 mx-auto  w-full max-w-5xl pt-16  xl:flex">
-      <div className="xl:mr-11">
+      <div className="xl:mr-11 xl:w-3/4">
         <Form />
         <Todolist />
       </div>
--- src/components/Todolist/Todolist.tsx
+++ src/components/Todolist/Todolist.tsx
@@ -5,12 +5,12 @@ import Todoitem from '../TodoItem/TodoItem';
 
 const Todolist = () => {
   return (
-    <div className="max-h-[50vh]  xl:w-2/5 xl:max-h-[80vh] overflow-y-auto p-4 mb-6 shadow-2xl rounded-lg">
+    <div className="max-h-[50vh] xl:max-h-[80vh] overflow-y-auto p-4 mb-6 shadow-2xl rounded-lg">
       {todos.total === 0 && <p>No todos added yet...</p>}
       <ul>
         {todos.todos
           .filter(todo =>
-            todo.name.toLowerCase().includes(todos.filter.toLowerCase()),
+            todo.name.toLowerCase().includes(todos.filter.toLowerCase())
           )
           .map(todo => {
             return (
